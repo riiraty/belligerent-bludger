c6710e3bffce8edeb6d953f3f1e408d121303a44
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-05-15 17:26:04 +0300

    init project and add some configurations

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+      System.out.println("Hello tiralabra!");
+
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Give message: ");
+      String message = scanner.nextLine();
+      scanner.close();
+
+      System.out.println("encrypting '" + message + "' ...");
+
+  }
+}
