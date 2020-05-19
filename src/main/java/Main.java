7ccfe4607da94c848a37d3ca6c399ed7e9e602d6
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-05-19 12:30:24 +0300

    config checkstyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,16 @@
+
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-      System.out.println("Hello tiralabra!");
+    public static void main(String[] args) {
+        System.out.println("Hello tiralabra!");
 
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Give message: ");
-      String message = scanner.nextLine();
-      scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give message: ");
+        String message = scanner.nextLine();
+        scanner.close();
 
-      System.out.println("encrypting '" + message + "' ...");
+        System.out.println("encrypting '" + message + "' ...");
 
-  }
+    }
 }
