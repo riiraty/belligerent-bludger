49487d6cdaa003b4a9cfa63b92519cd751b2c34e
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-06-03 17:28:32 +0300

    add Base64 encoding, update/implement needed methods

--- src/main/java/riiraty/cipher/MyRSA.java
+++ src/main/java/riiraty/cipher/MyRSA.java
@@ -1,6 +1,7 @@
 package riiraty.cipher;
 
 import java.math.BigInteger;
+import java.util.Base64;
 import riiraty.keys.KeyPair;
 
 /**
@@ -9,6 +10,11 @@ import riiraty.keys.KeyPair;
 public class MyRSA {
     private KeyPair keyPair;
 
+    /**
+     * Constructor sets the KeyPair used for cryption.
+     * 
+     * @param keyPair
+     */
     public MyRSA(KeyPair keyPair) {
         this.keyPair = keyPair;
     }
@@ -17,63 +23,88 @@ public class MyRSA {
         // for testing
     }
 
-    /***
-     * Converts user input of plain text to String of ASCII-character values
-     * Then converts formed String to BigInteger
-     * @param messageAsString user input
-     * @return ciphered message
+    /**
+     * This method encrypts a String with RSA public key.
+     * Base64 encoding is used for the ciphered BigInt.
+     * 
+     * @param msg as String
+     * @return ciphered message encoded to Base64
      */
-    public BigInteger StringToBigInt(String messageAsString) {
-        String cipherString = "";
-        int i = 0;
-        while (i < messageAsString.length()) {
-            int character = (int) messageAsString.charAt(i);
-            cipherString = cipherString + character;
-            i++;
-        }
-        BigInteger ciphered = new BigInteger(String.valueOf(cipherString));
-        return ciphered;
+    public String encrypt(String msg) {
+        BigInteger msgBigInt = messageToBigInt(msg);
+        BigInteger encrypted = msgBigInt.modPow(keyPair.getPublicKey().getPublicExponent(), 
+                                                keyPair.getPublicKey().getModulus());
+        String cipher = encodeBase64(encrypted.toString());
+
+        return cipher;
     }
 
     /**
-     * Encrypts the BigInteger form of message
-     * @param messageAsBigInteger
-     * @return encrypted cipher
-     */
-    public BigInteger encrypt(BigInteger messageAsBigInteger) {
-        BigInteger encrypted = messageAsBigInteger.modPow(keyPair.getPublic(), 
-                                                        keyPair.getModulus());
-        return encrypted;
-    }
-
-    /**
-     * Decrypts the BigInteger form of encrypted message
-     * @param cipherMessageAsBigInteger
-     * @return decrypted cipher
-     */
-    public BigInteger decrypt(BigInteger cipherMessageAsBigInteger) {
-        BigInteger decrypted = cipherMessageAsBigInteger.modPow(keyPair.getPrivate(), 
-                                                                keyPair.getModulus());
-        return decrypted;
-    }
-
-    /**
-     * Converts BigInteger.toString() to a String of 
-     * corresponding ASCII-characters
-     * @param cipherMessageAsBigInt
+     * This method decrypts a cipher String with RSA private key.
+     * Base64 decoding is used to reverse the encoding done while encrypting.
+     * 
+     * @param cipher
      * @return
      */
-    public String BigIntToString(BigInteger cipherMessageAsBigInt) {
-        String cipherAsString = cipherMessageAsBigInt.toString();
-        String plainText = "";
-        int i = 0;
-        while (i+2 <= cipherAsString.length()) {
-            int valueOfChar = Integer.parseInt(cipherAsString.substring(i, i+2));
-            char character = (char) valueOfChar;
-            plainText = plainText + character;
-            i += 2;
-        }
-        return plainText;
+    public String decrypt(String cipher) {
+        String decoded = decodeBase64(cipher);
+        BigInteger asBigInt = new BigInteger(decoded);
+        BigInteger decrypted = asBigInt.modPow(keyPair.getPrivateKey().getPrivateExponent(), 
+                                               keyPair.getPrivateKey().getModulus());
+        String message = BigIntToString(decrypted);
+
+        return message;
+    }
+
+    /**
+     * Converts String to BigInt.
+     * 
+     * @param message as String
+     * @return message as BigInt
+     */
+    public BigInteger messageToBigInt(String message) {
+        byte[] bytes = message.getBytes();
+        BigInteger msgBigInt = new BigInteger(bytes);
+
+        return msgBigInt;
+    }
+
+    /**
+     * Converts BigInt to String.
+     * 
+     * @param cipher
+     * @return
+     */
+    public String BigIntToString(BigInteger cipher) {
+        byte[] bytes = cipher.toByteArray();
+        String msgString = new String(bytes);
+
+        return msgString;
+    }
+
+    /**
+     * Encodes String with Base64.
+     * 
+     * @param arg 
+     * @return
+     */
+    public String encodeBase64(String arg) {
+        String encoded = Base64.getEncoder().encodeToString(arg.getBytes());
+
+        return encoded;
+    }
+
+    /**
+     * Decodes String with Base64.
+     * 
+     * @param arg
+     * @return
+     */
+    public String decodeBase64(String arg) {
+        byte[] bytes = Base64.getDecoder().decode(arg);
+        String decoded = new String(bytes);
+
+        return decoded;
     }
     
 }
--- src/main/java/riiraty/ui/CipherUi.java
+++ src/main/java/riiraty/ui/CipherUi.java
@@ -3,6 +3,8 @@ package riiraty.ui;
 import java.math.BigInteger;
 import java.util.Scanner;
 import riiraty.cipher.MyRSA;
+import riiraty.keys.PublicKey;
+import riiraty.keys.PrivateKey;
 import riiraty.keys.KeyPair;
 import riiraty.keys.KeyPairGenerator;
 
@@ -10,7 +12,6 @@ import riiraty.keys.KeyPairGenerator;
  * Command Line Interface of the cryptosystem.
  */
 public class CipherUi {
-
     private final Scanner scanner;
     private MyRSA crypter;
 
@@ -20,9 +21,11 @@ public class CipherUi {
 
     public void start() {
         // // Generated with riiraty.keys.KeyPairGenerator, keysize 32 bit
-        KeyPair keyPair = new KeyPair(BigInteger.valueOf(2019302167), //modulus
-                                    BigInteger.valueOf(1004236379), // e 
-                                    BigInteger.valueOf(253632059)); // d
+        PublicKey publicKey = new PublicKey(BigInteger.valueOf(2019302167), 
+                                            BigInteger.valueOf(1004236379));
+        PrivateKey privateKey = new PrivateKey(BigInteger.valueOf(2019302167), 
+                                               BigInteger.valueOf(253632059));
+        KeyPair keyPair = new KeyPair(publicKey, privateKey);
         crypter = new MyRSA(keyPair);
         System.out.println("Hello agent.");
         mainMenu();
@@ -44,28 +47,22 @@ public class CipherUi {
             String command = scanner.nextLine();
 
             if (command.equals("d")) {
+                System.out.print("Give cipher: ");
+                String cipher = scanner.nextLine();
+
                 System.out.println("--- decrypting ---");
+
+                String decrypted = crypter.decrypt(cipher);
+                System.out.println("Ciphered message: " + decrypted);
+
             } else if (command.equals("e")) {
-                System.out.println("Only uppercase allowed.");
                 System.out.print("Your message: ");
                 String message = scanner.nextLine();
 
                 System.out.println("--- encrypting ---");
 
-                BigInteger messageAsBigInt = crypter.StringToBigInt(message);
-                System.out.println("Message as BigInt: " + messageAsBigInt);
-
-                BigInteger encrypted = crypter.encrypt(messageAsBigInt);
-                System.out.println("Encrypted message: " + encrypted);
-
-                String encryptedString = crypter.BigIntToString(encrypted);
-                System.out.println("Encrypted as String: " + encryptedString);
-
-                BigInteger decrypted = crypter.decrypt(encrypted);
-                System.out.println("Decrypted message: " + decrypted);
-
-                String decryptedString = crypter.BigIntToString(decrypted);
-                System.out.println("Decrypted as String: " + decryptedString);
+                String cipher = crypter.encrypt(message);
+                System.out.println("Ciphered message: " + cipher);
 
             } else if (command.equals("keygen")) {
                 System.out.println("--- generating keys ---");
@@ -76,10 +73,60 @@ public class CipherUi {
                 scanner.close();
                 System.out.println("Goodbye.");
                 break;
+            } else if (command.equals("test")) {
+                test();
+                break;
             } else {
                 System.out.println("Not a valid command.");
             }
         }
         
+    }
+
+    // secret command for manual testing
+    public void test() {
+        System.out.println("----TEST------------------------------");
+        String message = "Test";
+
+        System.out.println("----Encryption------------------------");
+        System.out.println("Original message: " + message);
+        BigInteger bigI = crypter.messageToBigInt(message);
+        System.out.println("Message as BigInt: " + bigI);
+
+        BigInteger encrypted = bigI.modPow(BigInteger.valueOf(1004236379), 
+                                           BigInteger.valueOf(2019302167));
+        System.out.println("Encrypted BigInt: " + encrypted);
+
+        String encoded = crypter.encodeBase64(encrypted.toString());
+        System.out.println("Encoded to String: " + encoded);
+
+        System.out.println("--------------------------------------");
+
+        String cipher = crypter.encrypt(message);
+        System.out.println("Encrypted with crypter: " + cipher);
+
+        System.out.println("----Decryption------------------------");
+
+        System.out.println("Cipher to decrypt: " + cipher);
+
+        String decoded = crypter.decodeBase64(cipher);
+        System.out.println("Decoded cipher as String: " + decoded);
+
+        BigInteger asBigInt = new BigInteger(decoded);
+        System.out.println("String to BigInt: " + asBigInt);
+
+        BigInteger decryptedBigInt = asBigInt.modPow(BigInteger.valueOf(253632059), 
+                                               BigInteger.valueOf(2019302167));
+        System.out.println("Decrypted as BigInt: " + decryptedBigInt);
+
+        String decryptedCipher = crypter.BigIntToString(decryptedBigInt);
+        System.out.println("Decrypted as String: " + decryptedCipher);
+
+        System.out.println("--------------------------------------");
+
+        String decrypted = crypter.decrypt(cipher);
+        System.out.println("Decrypted with crypter: " + decrypted);
+
+        System.out.println("----FIN-------------------------------");
     }
 }
