2b1cffd1662e74edfdfc787dd4741e92c4af3923
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-06-09 16:05:32 +0300

    add String constructor

--- src/main/java/riiraty/math/BigInt.java
+++ src/main/java/riiraty/math/BigInt.java
@@ -11,8 +11,44 @@ public class BigInt {
      */
     final int[] mag;
 
-    public BigInt(String stringRep) {
-        this.mag = new int[stringRep.length()];
+    /**
+     * Translates String representation of BigInt in the radix
+     * 10 into a BigInt. 
+     * @param val String representation of BigInteger
+     */
+    public BigInt(String val) {
+        int cursor = 0;
+        int numDigits;
+        final int len = val.length();
+
+        //to do: check if negative
+
+        // skip leading zeros, get number of digits
+        while (cursor < len &&
+               Character.digit(val.charAt(cursor), 10) == 0) {
+            cursor++;
+        }
+        numDigits = len - cursor;
+
+        // String of zeros -> empty array
+        if (numDigits == 0) {
+            mag = new int[0];
+            return;
+        }
+
+        int[] magnitude = new int[numDigits];
+        for(int i = cursor; i < len; i++) {
+            magnitude[i] = Character.digit(val.charAt(cursor), 10);
+        }
+        mag = magnitude;
+    }
+
+    public String toString() {
+        String value = "";
+        for (int digit : mag){
+            value += digit;
+        }
+        return value;
     }
     
 }
