0b0afbc6350ee062f54a59f91d121c5095aa9477
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-06-27 16:54:53 +0300

    modify constructor for Crypter

--- src/main/java/riiraty/ui/CipherUi.java
+++ src/main/java/riiraty/ui/CipherUi.java
@@ -3,9 +3,6 @@ package riiraty.ui;
 import java.util.Scanner;
 
 import riiraty.cipher.Crypter;
-import riiraty.keys.PublicKey;
-import riiraty.keys.PrivateKey;
-import riiraty.keys.KeyPair;
 import riiraty.keys.KeyPairGenerator;
 import riiraty.util.Tester;
 
@@ -22,12 +19,7 @@ public class CipherUi {
     }
 
     public void start() {
-        // Default key pair of 1024 bit size, makes max input 128 chars
-        PublicKey publicKey = new PublicKey("MTA3OTYzNjUwMDU3ODc5NDUzMzA4NTQyOTgyMjcwMjIzODMyNjIxOTg3ODcwMzI2NDc0NTAzNjM0NTQzMDExMzQwNDE5NjI1MjUyNzM0OTIyNDMzNjIzMDk0MDEwNjk5NjU1NTQzMTU5NTMyMjAzNjc4MTE5OTQ2ODM2ODAwODg3NTgwNDU5MzkwODY5OTMzMjU5MjIxMDAzNTI5ODY3NDM0MzMzMjAzMjQyMjYwMjAxODczNjE2MDEwNDA5NzkwMDA4MjgxMjAyMzAwMDI1OTQ3MjYzMjQ4MzQzNDA5MDM3MjQyNjg1MjU0MDQwNDE0NzQ5MjY1MDY3MjgyMDEzMjM1MDIyNDUyODc0MzcxOTYyODM4MDY4NDUwMzEzMDQwNDMwODEwOTc4OTgzNDUxMzI4Mzg5OjE5MzkyMzQ5ODk1MDEwOTk3NDk0MjkwNTEzMjA4MzA4NjQzNzYzMTIzNjQwNDM3MzgyMTk3MTg4NTM2MDY0ODA3MzM2MzIwODM1NDM2ODMyODM2NDEwNTIzMTU3MjMwOTgzNjc0NDQwMDI0NDM5OTIwOTgxMjQ5NjIwNDM1MTIzMDE0NTEzMTU2NjU2OTkzOTQxMDQxMzA0ODMzODAyNTU5NjgzODI4Mzk2NTAyNjA0NjA1MTk0MzQ3NjE0NTczOTg1MzY3MTA0NDU2NjA3ODE3MzkwNTcyNjcyODAyODYwNTM4Mzk1MzgwOTI3MTE4Njc1OTc3NjY0Mzk0MTg2OTY4NDQyNTk0Njg0MDA3MTQ3Njc4MzUyODAyOTQ1MTU3NzQxNDgxODc5MDUzNTU0MTY0ODQx");
-        PrivateKey privateKey = new PrivateKey("MTA3OTYzNjUwMDU3ODc5NDUzMzA4NTQyOTgyMjcwMjIzODMyNjIxOTg3ODcwMzI2NDc0NTAzNjM0NTQzMDExMzQwNDE5NjI1MjUyNzM0OTIyNDMzNjIzMDk0MDEwNjk5NjU1NTQzMTU5NTMyMjAzNjc4MTE5OTQ2ODM2ODAwODg3NTgwNDU5MzkwODY5OTMzMjU5MjIxMDAzNTI5ODY3NDM0MzMzMjAzMjQyMjYwMjAxODczNjE2MDEwNDA5NzkwMDA4MjgxMjAyMzAwMDI1OTQ3MjYzMjQ4MzQzNDA5MDM3MjQyNjg1MjU0MDQwNDE0NzQ5MjY1MDY3MjgyMDEzMjM1MDIyNDUyODc0MzcxOTYyODM4MDY4NDUwMzEzMDQwNDMwODEwOTc4OTgzNDUxMzI4Mzg5Ojg4NjY5MTUzOTY3MzIzNDY0MDUxOTcwNzEzMTY3NTc1NjUxMDUyMTAwMTgyMjM1MDg3MzU0NTE1MTUxODczMzQ0MjYyNzM0OTA4NjE5Mzk4NTIzOTg5MTczMjY5ODM2MzQ5ODgyNzYxNjQwOTQzNTQ5ODQ5MjU1NTE5MDQxMDEzNTIwODUyNzM4ODMxNjcxMzM5NTcyMDc4OTM3MTM5MDc3Njc1NzY4NDExNDY0Mzc1MzQzNjU2Mzc4MjUzNDg5MjkyNzk0OTYzODYzNjkxMDYwOTk3MzE5NjgwNzc1MDk1MjE4MjE0MDc0NzU5NDUyOTEwNTk2MTkzNzkxNjQxOTc5NDYyMzUyMDEyODUyNDk5MTYwMDUwNzk0MDYyNDQxMTM0OTM0NTY3NDk0Nzg3Mzg2Mzkz");
-        KeyPair keyPair = new KeyPair(publicKey, privateKey);
-
-        crypter = new Crypter(keyPair);
+        crypter = new Crypter();
         keyPairGenerator = new KeyPairGenerator();
 
         System.out.println("Hello agent.");
@@ -116,7 +108,7 @@ public class CipherUi {
 
     public void keygen() {
         try {
-            System.out.println("-------------generating keys--------------");
+            System.out.println("-------------\u001b[32mgenerating keys\u001b[0m--------------");
             keyPairGenerator.keygen();
             System.out.println(keyPairGenerator.getKeys().toString());
         } catch (Exception e) {
--- src/test/java/riiraty/cipher/TestCrypter.java
+++ src/test/java/riiraty/cipher/TestCrypter.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigInteger;
+
 import riiraty.keys.PublicKey;
 import riiraty.keys.PrivateKey;
 import riiraty.keys.KeyPair;
@@ -18,10 +19,12 @@ public class TestCrypter {
     public void setUp() {
         this.crypter = new Crypter();
 
-        PublicKey publicKey = new PublicKey("MTA3OTYzNjUwMDU3ODc5NDUzMzA4NTQyOTgyMjcwMjIzODMyNjIxOTg3ODcwMzI2NDc0NTAzNjM0NTQzMDExMzQwNDE5NjI1MjUyNzM0OTIyNDMzNjIzMDk0MDEwNjk5NjU1NTQzMTU5NTMyMjAzNjc4MTE5OTQ2ODM2ODAwODg3NTgwNDU5MzkwODY5OTMzMjU5MjIxMDAzNTI5ODY3NDM0MzMzMjAzMjQyMjYwMjAxODczNjE2MDEwNDA5NzkwMDA4MjgxMjAyMzAwMDI1OTQ3MjYzMjQ4MzQzNDA5MDM3MjQyNjg1MjU0MDQwNDE0NzQ5MjY1MDY3MjgyMDEzMjM1MDIyNDUyODc0MzcxOTYyODM4MDY4NDUwMzEzMDQwNDMwODEwOTc4OTgzNDUxMzI4Mzg5OjE5MzkyMzQ5ODk1MDEwOTk3NDk0MjkwNTEzMjA4MzA4NjQzNzYzMTIzNjQwNDM3MzgyMTk3MTg4NTM2MDY0ODA3MzM2MzIwODM1NDM2ODMyODM2NDEwNTIzMTU3MjMwOTgzNjc0NDQwMDI0NDM5OTIwOTgxMjQ5NjIwNDM1MTIzMDE0NTEzMTU2NjU2OTkzOTQxMDQxMzA0ODMzODAyNTU5NjgzODI4Mzk2NTAyNjA0NjA1MTk0MzQ3NjE0NTczOTg1MzY3MTA0NDU2NjA3ODE3MzkwNTcyNjcyODAyODYwNTM4Mzk1MzgwOTI3MTE4Njc1OTc3NjY0Mzk0MTg2OTY4NDQyNTk0Njg0MDA3MTQ3Njc4MzUyODAyOTQ1MTU3NzQxNDgxODc5MDUzNTU0MTY0ODQx");
-        PrivateKey privateKey = new PrivateKey("MTA3OTYzNjUwMDU3ODc5NDUzMzA4NTQyOTgyMjcwMjIzODMyNjIxOTg3ODcwMzI2NDc0NTAzNjM0NTQzMDExMzQwNDE5NjI1MjUyNzM0OTIyNDMzNjIzMDk0MDEwNjk5NjU1NTQzMTU5NTMyMjAzNjc4MTE5OTQ2ODM2ODAwODg3NTgwNDU5MzkwODY5OTMzMjU5MjIxMDAzNTI5ODY3NDM0MzMzMjAzMjQyMjYwMjAxODczNjE2MDEwNDA5NzkwMDA4MjgxMjAyMzAwMDI1OTQ3MjYzMjQ4MzQzNDA5MDM3MjQyNjg1MjU0MDQwNDE0NzQ5MjY1MDY3MjgyMDEzMjM1MDIyNDUyODc0MzcxOTYyODM4MDY4NDUwMzEzMDQwNDMwODEwOTc4OTgzNDUxMzI4Mzg5Ojg4NjY5MTUzOTY3MzIzNDY0MDUxOTcwNzEzMTY3NTc1NjUxMDUyMTAwMTgyMjM1MDg3MzU0NTE1MTUxODczMzQ0MjYyNzM0OTA4NjE5Mzk4NTIzOTg5MTczMjY5ODM2MzQ5ODgyNzYxNjQwOTQzNTQ5ODQ5MjU1NTE5MDQxMDEzNTIwODUyNzM4ODMxNjcxMzM5NTcyMDc4OTM3MTM5MDc3Njc1NzY4NDExNDY0Mzc1MzQzNjU2Mzc4MjUzNDg5MjkyNzk0OTYzODYzNjkxMDYwOTk3MzE5NjgwNzc1MDk1MjE4MjE0MDc0NzU5NDUyOTEwNTk2MTkzNzkxNjQxOTc5NDYyMzUyMDEyODUyNDk5MTYwMDUwNzk0MDYyNDQxMTM0OTM0NTY3NDk0Nzg3Mzg2Mzkz");
+        PublicKey publicKey = new PublicKey(BigInteger.valueOf(2019302167), 
+                                            BigInteger.valueOf(1004236379));
+        PrivateKey privateKey = new PrivateKey(BigInteger.valueOf(2019302167), 
+                                               BigInteger.valueOf(253632059));
         KeyPair keyPair = new KeyPair(publicKey, privateKey);
-        this. keyCrypter = new Crypter(keyPair);
+        this.keyCrypter = new Crypter(keyPair);
     }
     
     @Test
@@ -42,14 +45,14 @@ public class TestCrypter {
 
     @Test 
     public void encryptionWithDefaultKeyWorks() {
-        String encrypted = keyCrypter.encrypt("Test", "");
+        String encrypted = crypter.encrypt("Test", "");
 
         assertEquals("NTQzMjY3MzAyODkxMzg5ODU0MjEyODA4MDUxODA4MjgxNzc1MTI1MDYxMjQ4MjgzOTE0NzEzOTQ1NjQyNzE3NTM1MzA4MjQzNDE3NjM1MTU4OTA3NDkyMjE2MTQ1MzA0NDQxOTYwNjI4MzkxNzUzMjE0NDY0MTU5ODgyMTU4MDMwODQ4NjQwMjM3NDIzMjMwOTk2NTY1MzUxMDU5MTU4MzQwMzE3ODUyNzIzNzA4Mzg4NDY2OTYwMTQ0NzQ3NjkxMTYwMjgzNDgyNjI1NDI2NjM5MDUwNTk4NTYxOTYzMzM0NjQwODc2NDc1MzQwODM3ODEyMTQ1NDQ1MDI2NjM4OTc5ODQyMTkxNzQ1MTk4NTA4ODY5MjkyMzYwODAwNzU2NDA5NjE1Mjg4MzU3NDc2OTUwMzI=", encrypted);
     }
 
     @Test
     public void decryptionWithDefaultKeyWorks() {
-        String decrypted = keyCrypter.decrypt("NTQzMjY3MzAyODkxMzg5ODU0MjEyODA4MDUxODA4MjgxNzc1MTI1MDYxMjQ4MjgzOTE0NzEzOTQ1NjQyNzE3NTM1MzA4MjQzNDE3NjM1MTU4OTA3NDkyMjE2MTQ1MzA0NDQxOTYwNjI4MzkxNzUzMjE0NDY0MTU5ODgyMTU4MDMwODQ4NjQwMjM3NDIzMjMwOTk2NTY1MzUxMDU5MTU4MzQwMzE3ODUyNzIzNzA4Mzg4NDY2OTYwMTQ0NzQ3NjkxMTYwMjgzNDgyNjI1NDI2NjM5MDUwNTk4NTYxOTYzMzM0NjQwODc2NDc1MzQwODM3ODEyMTQ1NDQ1MDI2NjM4OTc5ODQyMTkxNzQ1MTk4NTA4ODY5MjkyMzYwODAwNzU2NDA5NjE1Mjg4MzU3NDc2OTUwMzI=", "");
+        String decrypted = crypter.decrypt("NTQzMjY3MzAyODkxMzg5ODU0MjEyODA4MDUxODA4MjgxNzc1MTI1MDYxMjQ4MjgzOTE0NzEzOTQ1NjQyNzE3NTM1MzA4MjQzNDE3NjM1MTU4OTA3NDkyMjE2MTQ1MzA0NDQxOTYwNjI4MzkxNzUzMjE0NDY0MTU5ODgyMTU4MDMwODQ4NjQwMjM3NDIzMjMwOTk2NTY1MzUxMDU5MTU4MzQwMzE3ODUyNzIzNzA4Mzg4NDY2OTYwMTQ0NzQ3NjkxMTYwMjgzNDgyNjI1NDI2NjM5MDUwNTk4NTYxOTYzMzM0NjQwODc2NDc1MzQwODM3ODEyMTQ1NDQ1MDI2NjM4OTc5ODQyMTkxNzQ1MTk4NTA4ODY5MjkyMzYwODAwNzU2NDA5NjE1Mjg4MzU3NDc2OTUwMzI=", "");
 
         assertEquals("Test", decrypted);;
     }
@@ -57,7 +60,7 @@ public class TestCrypter {
     @Test 
     public void encryptionUsesGivenKey() {
         // PublicKey(BigInteger.valueOf(2019302167), BigInteger.valueOf(1004236379));
-        String encrypted = keyCrypter.encrypt("Test", "MjAxOTMwMjE2NzoxMDA0MjM2Mzc5");
+        String encrypted = crypter.encrypt("Test", "MjAxOTMwMjE2NzoxMDA0MjM2Mzc5");
 
         assertEquals("NDcxODgxNjA=", encrypted);
     }
@@ -65,7 +68,7 @@ public class TestCrypter {
     @Test
     public void decrypterUsesGivenKey() {
         // PrivateKey(BigInteger.valueOf(2019302167), BigInteger.valueOf(253632059));
-        String decrypted = keyCrypter.decrypt("NDcxODgxNjA=", "MjAxOTMwMjE2NzoyNTM2MzIwNTk=");
+        String decrypted = crypter.decrypt("NDcxODgxNjA=", "MjAxOTMwMjE2NzoyNTM2MzIwNTk=");
 
         assertEquals("Test", decrypted);;
     }
@@ -74,8 +77,8 @@ public class TestCrypter {
     public void whitespacesRemainAsOriginal() {
         String original = "  This is  a VERY secret  msg     .  ";
 
-        String encrypted = keyCrypter.encrypt(original, "");
-        String decrypted = keyCrypter.decrypt(encrypted, "");
+        String encrypted = crypter.encrypt(original, "");
+        String decrypted = crypter.decrypt(encrypted, "");
 
         assertEquals(original, decrypted);
     }
@@ -84,10 +87,22 @@ public class TestCrypter {
     public void BasicCharactersRemainAsOriginal() {
         String original = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-        String encrypted = keyCrypter.encrypt(original, "");
-        String decrypted = keyCrypter.decrypt(encrypted, "");
+        String encrypted = crypter.encrypt(original, "");
+        String decrypted = crypter.decrypt(encrypted, "");
 
         assertEquals(original, decrypted);
+    }
+
+    @Test
+    public void keyCrypterUsesGivenPublicKeyAsDefault() {
+        String encrypted = keyCrypter.encrypt("Test", "");
+        assertEquals("NDcxODgxNjA=", encrypted);
+    }
+
+    @Test
+    public void keyCrypterUsesGivenPrivateKeyAsDefault() {
+        String decrypted = keyCrypter.decrypt("NDcxODgxNjA=", "");
+        assertEquals("Test", decrypted);
     }
 
 }
